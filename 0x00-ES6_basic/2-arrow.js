bc45fed67cd7559262016ceab106cd9634ea8eea
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-13 21:25:11 +0200

    Refactor addNeighborhood function to use arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;    // eslint-disable-line
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
