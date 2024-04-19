89de1ef6e61cb8d4609c7a99da03dabb4239ebae
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-19 08:57:27 +0200

    Add new files and update existing files in 0x01-ES6_promise directory

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
