9790012727f1d2dfec9408ed020a047632f3b441
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-25 16:41:26 +0200

    Refactor guardrail function in 0x01-ES6_promise/9-try.js - Update error handling and return queue of results

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 export default function guardrail(mathFunction) {
-  let queue = [];
+  const queue = [];
   try {
     queue.push(mathFunction());
   } catch (error) {
     queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
-    return queue;
   }
+  return queue;
 }
