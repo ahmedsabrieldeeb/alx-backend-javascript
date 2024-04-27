225508689622304b1cba4467307bc48b16dd828a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-27 14:19:22 +0300

    Add HolbertonCourse class to 0x02-ES6_classes/2-hbtn_course.js - Implement a class for Holberton courses

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,44 @@
-#!/usr/bin/node
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = value;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = value;
+    }
+  }
+}
