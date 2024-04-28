5adf21fbcbdb4d34be519c280b37876bd18f98e2
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-28 08:16:53 +0300

    Update Pricing class in 0x02-ES6_classes/4-pricing.js - Add getters, setters, and displayFullPrice method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,45 @@
-#!/usr/bin/node
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency === false) {
+      throw new TypeError('Currency must be a Currency');
+    } else {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
