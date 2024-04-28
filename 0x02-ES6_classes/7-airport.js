10c3e120598fd65f86b58109ebf33ada273364e1
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-28 12:16:28 +0300

    Update Airport class in 0x02-ES6_classes/7-airport.js - Add constructor and toString method

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
