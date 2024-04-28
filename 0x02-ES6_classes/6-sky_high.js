e4db40231cf09e4818da6a2cdd1b5156e769fcef
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-28 12:09:09 +0300

    Add SkyHighBuilding class to 0x02-ES6_classes/6-sky_high.js - Implement a class for sky-high buildings

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
