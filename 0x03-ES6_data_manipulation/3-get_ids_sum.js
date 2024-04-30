2bb4de7369a378adb0074041b2483006ca4ae8b4
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-30 21:48:08 +0300

    Add getStudentIdsSum function to 0x03-ES6_data_manipulation/3-get_ids_sum.js - Implement a function to calculate the sum of student IDs

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+export default function getStudentIdsSum(array) {
+  if (!(array instanceof Array)) return -1;// no array has passed
+
+  return array.reduce((x, y) => x + y.id, 0);
+}
