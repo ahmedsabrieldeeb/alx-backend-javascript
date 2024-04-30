810f78c596367ae5a44b35c575cf76f8e7f887da
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-04-30 22:58:51 +0300

    Update updateStudentGradeByCity function to modify student grades by city in 0x03-ES6_data_manipulation/4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const filtered = students.filter((objectValue) => objectValue.location === city);
+
+  const result = filtered.map((objectValue) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === objectValue.id);
+    const newObjectValue = { ...objectValue };// to avoid no-param-reassign for eslint
+
+    if (newGrade !== undefined) newObjectValue.grade = newGrade.grade;
+    else newObjectValue.grade = 'N/A';
+
+    return newObjectValue;
+  });
+
+  return result;
+}
