cdf5ed23734a80b5a5981acab4d6bc572d43437e
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-05-01 22:07:19 +0300

    Fix bug in cleanSet function to handle empty strings in 0x03-ES6_data_manipulation/8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startsString) {
   if (startsString === '') return startsString;
 
-  const filtered = [...set].filter((str) => str.startsWith(startsString));
+  const filtered = [...set].filter((str) => str && str.startsWith(startsString));
   const cleaned = filtered.map((str) => str.slice(startsString.length));
   return cleaned.join('-');
 }
