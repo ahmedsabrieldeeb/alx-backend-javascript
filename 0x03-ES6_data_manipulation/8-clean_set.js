abd0f506a5231a588040954ea1d26d8fca0f6be5
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-05-01 22:02:41 +0300

    Refactor cleanSet function to improve readability and performance in 0x03-ES6_data_manipulation/8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function cleanSet(set, startsString) {
+  if (startsString === '') return startsString;
+
+  const filtered = [...set].filter((str) => str.startsWith(startsString));
+  const cleaned = filtered.map((str) => str.slice(startsString.length));
+  return cleaned.join('-');
+}
