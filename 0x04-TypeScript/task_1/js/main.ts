0173fd1735664fb85bedf67591c1462099c1d23e
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-06 02:55:54 +0300

    Add TypeScript interfaces for Directors and Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,12 @@
-// firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
-// fullTimeEmployee(boolean) this attribute should always be defined
-// yearsOfExperience(number) this attribute is optional
-// location(string) this attribute should always be defined
-// Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
-
+/*
+*** firstName(string) and lastName(string).
+***     These two attributes should only be modifiable when a Teacher is first initialized
+*** fullTimeEmployee(boolean) this attribute should always be defined
+*** yearsOfExperience(number) this attribute is optional
+*** location(string) this attribute should always be defined
+*** Add the possibility to add any attribute to the Object like contract(boolean,
+*** without specifying the name of the attribute.
+*/
 interface Teacher {
   [key: string]: any;
 
@@ -12,4 +15,25 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
+}
+
+/** Write an interface named Directors that extends Teacher.
+*** It requires an attribute named numberOfReports(number).
+*/
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+/**  Write a function printTeacher:
+***  It accepts two arguments firstName and lastName
+***  It returns the first letter of the firstName and the full lastName
+*** Example: printTeacher("John", "Doe") -> J. Doe
+*/
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+/* Write an interface for the function named printTeacherFunction. */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
